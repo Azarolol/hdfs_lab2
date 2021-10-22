a6df0899e877b343afe966b802fc0f2910802b96
Azarolol <gimazovazamat@mail.ru>
2021-10-22 13:56:59 +0300

    Scripted auto-commit on change (2021-10-22 13:56:59) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportWritable.java
+++ src/main/java/hdfs/lab2/azarolol/AirportWritable.java
@@ -4,5 +4,10 @@ public class AirportWritable {
     private final String name;
     private final int ID;
 
+    public Airport(String name, int ID) {
+        this.ID = ID;
+        this.name = name;
+    }
+
     
 }
