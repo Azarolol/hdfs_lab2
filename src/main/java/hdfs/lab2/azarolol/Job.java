5353336812f2636d506913e6e209446edc30ca8b
Azarolol <gimazovazamat@mail.ru>
2021-10-22 13:23:48 +0300

    Scripted auto-commit on change (2021-10-22 13:23:48) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/Job.java
+++ src/main/java/hdfs/lab2/azarolol/Job.java
@@ -2,8 +2,9 @@ package hdfs.lab2.azarolol;
 
 import org.apache.hadoop.mapreduce.Job;
 
-public class Job {
+public class A {
     public static void main(String[] args) {
-        
+        Job job = Job.getInstance();
+        job.set
     }
 }
