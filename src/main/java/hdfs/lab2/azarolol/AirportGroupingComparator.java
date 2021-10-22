087820c44ed2f96e650fc56cc541c12efb1f9703
Azarolol <gimazovazamat@mail.ru>
2021-10-22 16:29:49 +0300

    Scripted auto-commit on change (2021-10-22 16:29:49) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportGroupingComparator.java
+++ src/main/java/hdfs/lab2/azarolol/AirportGroupingComparator.java
@@ -4,6 +4,10 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
+    public AirportGroupingComparator() {
+        super()
+    }
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         AirportWritableComparable airportWritableComparable1 = (AirportWritableComparable) a;
