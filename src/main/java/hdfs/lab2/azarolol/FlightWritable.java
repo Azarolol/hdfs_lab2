0ca4bd9384c7c7a2e38fb75da6eaef0f0864354d
Azarolol <gimazovazamat@mail.ru>
2021-10-22 14:39:03 +0300

    Scripted auto-commit on change (2021-10-22 14:39:03) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/FlightWritable.java
+++ src/main/java/hdfs/lab2/azarolol/FlightWritable.java
@@ -2,9 +2,11 @@ package hdfs.lab2.azarolol;
 
 public class FlightWritable {
     private final int destinationAirportID;
+    private final int delayTime;
 
-    public FlightWritable(int destinationAirportID) {
+    public FlightWritable(int destinationAirportID, int delayTime) {
         this.destinationAirportID = destinationAirportID;
+        this.delayTime = delayTime;
     }
 
     public int getDestinationAirportID() {
