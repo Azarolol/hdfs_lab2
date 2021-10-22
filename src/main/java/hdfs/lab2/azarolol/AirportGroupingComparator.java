b2911ced44dafb09a7cf5c0e39e19dec57e7483b
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:05:46 +0300

    Scripted auto-commit on change (2021-10-22 15:05:46) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportGroupingComparator.java
+++ src/main/java/hdfs/lab2/azarolol/AirportGroupingComparator.java
@@ -3,4 +3,13 @@ package hdfs.lab2.azarolol;
 import org.apache.hadoop.io.RawComparator;
 
 public class AirportGroupingComparator implements RawComparator {
+    @Override
+    public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
+        return 0;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
 }
