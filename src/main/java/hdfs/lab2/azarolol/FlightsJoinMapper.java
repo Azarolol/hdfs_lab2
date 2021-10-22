a24b50d21afe1404dcbcea6e1004cebee631fed8
Azarolol <gimazovazamat@mail.ru>
2021-10-22 14:23:43 +0300

    Scripted auto-commit on change (2021-10-22 14:23:43) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/FlightsJoinMapper.java
+++ src/main/java/hdfs/lab2/azarolol/FlightsJoinMapper.java
@@ -3,5 +3,6 @@ package hdfs.lab2.azarolol;
 import org.apache.mapreduce.Mapper;
 
 public class FlightsJoinMapper extends Mapper<Writable, Text, AirportWritableComparable, Text> {
+    @Override
     
 }
