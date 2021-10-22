94247eb225c3b0eaba8d33452b47c5f5a76c2041
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:04:31 +0300

    Scripted auto-commit on change (2021-10-22 15:04:31) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportPartitioner.java
+++ src/main/java/hdfs/lab2/azarolol/AirportPartitioner.java
@@ -7,6 +7,6 @@ public class AirportPartitioner extends Partitioner<AirportWritableComparable, T
 
     @Override
     public int getPartition(AirportWritableComparable airportWritableComparable, Text text, int i) {
-        return airportWritableComparable.getID() % 
+        return airportWritableComparable.getID() % i;
     }
 }
