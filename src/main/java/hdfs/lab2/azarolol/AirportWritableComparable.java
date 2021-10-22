8c0fa155577abfbed1a54e0e58ea4db6fec33479
Azarolol <gimazovazamat@mail.ru>
2021-10-22 14:16:52 +0300

    Scripted auto-commit on change (2021-10-22 14:16:52) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportWritableComparable.java
+++ src/main/java/hdfs/lab2/azarolol/AirportWritableComparable.java
@@ -2,4 +2,12 @@ package hdfs.lab2.azarolol;
 
 import org.apache.hadoop.io.WritableComparable;
 
-public class AirportWritableComparable implements WritableComparable<AirportWritableComparable>
+public class AirportWritableComparable implements WritableComparable<AirportWritableComparable> {
+    private final int ID;
+    private final boolean indicator;
+
+    public AirportWritableComparable(int ID, boolean indicator) {
+        this.ID = ID;
+        this.indicator = indicator;
+    }
+}
