47613f5a3183017ffb97a7e2b7a17a577406a948
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:02:51 +0300

    Scripted auto-commit on change (2021-10-22 15:02:51) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportDelayTIme.java
+++ src/main/java/hdfs/lab2/azarolol/AirportDelayTIme.java
@@ -21,7 +21,7 @@ public class AirportDelayTime {
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsJoinMapper.class);
 
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setPartitionerClass(AirportPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
         job.setReducerClass(JoinReducer.class);
         job.setMapOutputKeyClass(TextPair.class);
---
+++ src/main/java/hdfs/lab2/azarolol/AirportPartitioner.java
@@ -0,0 +1,6 @@
+package hdfs.lab2.azarolol;
+
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirportPartitioner extends Partitioner {
+}
