de0591f58637ab073d24b6c27fff58fd423628d5
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:14:37 +0300

    Scripted auto-commit on change (2021-10-22 15:14:37) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportReducer.java
+++ src/main/java/hdfs/lab2/azarolol/AirportReducer.java
@@ -3,6 +3,13 @@ package hdfs.lab2.azarolol;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
 
+import java.io.IOException;
+import java.util.Iterator;
+
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
-    
+    @Override
+    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        
+    }
 }
