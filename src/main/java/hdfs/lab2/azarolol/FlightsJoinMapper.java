8f73f4e2517d3380affadeec3b0b48c240d4625b
Azarolol <gimazovazamat@mail.ru>
2021-10-22 14:31:22 +0300

    Scripted auto-commit on change (2021-10-22 14:31:22) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/FlightsJoinMapper.java
+++ src/main/java/hdfs/lab2/azarolol/FlightsJoinMapper.java
@@ -5,6 +5,7 @@ import org.apache.mapreduce.Mapper;
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) {
-        Flight mappingFlight = FlightWritable.read(value.toString().)
+        Flight mappingFlight = FlightWritable.read(value.toString().replaceAll("\"", ""));
+        
     }
 }
