b16b5e3068f0998c16d693657c754c82e39d60a5
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:12:41 +0300

    Scripted auto-commit on change (2021-10-22 15:12:41) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportReducer.java
+++ src/main/java/hdfs/lab2/azarolol/AirportReducer.java
@@ -1,6 +1,8 @@
 package hdfs.lab2.azarolol;
 
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.Text;
 
-public class AirportReducer extends Reducer<> {
+public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
+    
 }
