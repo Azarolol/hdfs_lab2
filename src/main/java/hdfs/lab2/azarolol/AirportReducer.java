4006c0a4669aaeb62d59ef9d26f0a8728c1c8f0a
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:19:45 +0300

    Scripted auto-commit on change (2021-10-22 15:19:45) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportReducer.java
+++ src/main/java/hdfs/lab2/azarolol/AirportReducer.java
@@ -11,6 +11,9 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text name = new Text(iter.next().toString());
-        
+        if (!iter.hasNext()) {
+            return;
+        }
+        float delay = 
     }
 }
