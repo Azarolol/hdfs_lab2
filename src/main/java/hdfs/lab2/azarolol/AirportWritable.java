76290b92679730820332ae15f0778266f3d659dc
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:00:18 +0300

    Scripted auto-commit on change (2021-10-22 15:00:18) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportWritable.java
+++ src/main/java/hdfs/lab2/azarolol/AirportWritable.java
@@ -1,10 +1,12 @@
 package hdfs.lab2.azarolol;
 
+import org.apache.hadoop.io.IntWritable;
+
 public class AirportWritable {
     private final String name;
-    private final int ID;
+    private final IntWritable ID;
 
-    public AirportWritable(String name, int ID) {
+    public AirportWritable(String name, IntWritable ID) {
         this.ID = ID;
         this.name = name;
     }
@@ -13,13 +15,13 @@ public class AirportWritable {
         return name;
     }
 
-    public int getID() {
+    public IntWritable getID() {
         return ID;
     }
 
     public static AirportWritable read(String input) {
         String[] fields = input.split(",");
-        int ID = Integer.parseInt(fields[0]);
+        IntWritable ID = new IntWritable(Integer.parseInt(fields[0]));
         String name = fields[1];
         return new AirportWritable(name, ID);
     }
