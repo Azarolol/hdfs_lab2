5f156ab13cb67bf30475cd14c9f2610e3fe4d7a0
Azarolol <gimazovazamat@mail.ru>
2021-10-22 13:30:59 +0300

    Scripted auto-commit on change (2021-10-22 13:30:59) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportDelayTIme.java
+++ src/main/java/hdfs/lab2/azarolol/AirportDelayTIme.java
@@ -5,6 +5,20 @@ import org.apache.hadoop.mapreduce.Job;
 public class AirportDelayTime {
     public static void main(String[] args) {
         Job job = Job.getInstance();
-        job.set
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("AirportDelayTime counter");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
