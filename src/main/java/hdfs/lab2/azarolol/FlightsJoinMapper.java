b4bf97fdf0639c6a8b8f67128bd421303b4d00e0
Azarolol <gimazovazamat@mail.ru>
2021-10-22 17:49:34 +0300

    Scripted auto-commit on change (2021-10-22 17:49:34) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/FlightsJoinMapper.java
+++ src/main/java/hdfs/lab2/azarolol/FlightsJoinMapper.java
@@ -11,10 +11,13 @@ import java.util.Objects;
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        FlightWritable mappingFlight = FlightWritable.read(value.toString().replaceAll("\"", ""));
-        IntWritable ID = mappingFlight.getDestinationAirportID();
-        if (ID.hashCode() != 0) {
-            context.write(new AirportWritableComparable(ID, new IntWritable(1)), new Text(String.valueOf(mappingFlight.getDelayTime())));
+        String valueTrimmed = value.toString().replaceAll("\"", "");
+        if (!(valueTrimmed.split(",")[18].equals(""))) {
+            FlightWritable mappingFlight = FlightWritable.read(valueTrimmed);
+            IntWritable ID = mappingFlight.getDestinationAirportID();
+            if (ID.hashCode() != 0) {
+                context.write(new AirportWritableComparable(ID, new IntWritable(1)), new Text(String.valueOf(mappingFlight.getDelayTime())));
+            }
         }
     }
 }
