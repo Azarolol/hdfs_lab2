e29c75083f9d9a53cc20625377a0c575a42b84d7
Azarolol <gimazovazamat@mail.ru>
2021-10-22 13:24:39 +0300

    Scripted auto-commit on change (2021-10-22 13:24:39) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportDelayTIme.java
+++ src/main/java/hdfs/lab2/azarolol/AirportDelayTIme.java
@@ -2,7 +2,7 @@ package hdfs.lab2.azarolol;
 
 import org.apache.hadoop.mapreduce.Job;
 
-public class A {
+public class AirportDelayTime {
     public static void main(String[] args) {
         Job job = Job.getInstance();
         job.set
