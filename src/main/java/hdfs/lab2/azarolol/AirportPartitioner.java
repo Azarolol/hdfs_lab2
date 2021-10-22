ce0bb439bd50da879d617fc51b897de3f146dbf5
Azarolol <gimazovazamat@mail.ru>
2021-10-22 15:03:21 +0300

    Scripted auto-commit on change (2021-10-22 15:03:21) by gitwatch.sh

--- src/main/java/hdfs/lab2/azarolol/AirportPartitioner.java
+++ src/main/java/hdfs/lab2/azarolol/AirportPartitioner.java
@@ -1,6 +1,12 @@
 package hdfs.lab2.azarolol;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class AirportPartitioner extends Partitioner {
+public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
+
+    @Override
+    public int getPartition(AirportWritableComparable airportWritableComparable, Text text, int i) {
+        return 0;
+    }
 }
